ca4b33fb71c21b7745844046330c67d226cfa271
t18 <ricareng1>
2021-08-09 11:17:51 -0600

    Included my_range.py & my_tuples.py

---
+++ my_tuples.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Author : t18 <me@wsu.com>
+Date   : 8/9/2021
+Purpose:
+"""
+
+
+def minmax(items):
+    """
+    Return the maximum and minimum of the collection
+    :param items: collection of objects
+    :return: min and max
+    """
+    return min(items), max(items)
+
+
+# --------------------------------------------------
+def main():
+    """Make your noise here"""
+
+    # A tuple of any kind of object
+    # t = ("Ogden", 1.99, 2)
+    t = "Ogden", 1.99, 2, 265e10  # optional parenthesis
+    print(t)  # all tuple
+    print(type(t))  # type of object
+    print(t[0])  # positional object
+    print(f'Tuple is {len(t)} items long')
+
+    # Iterate over tuple
+    for item in t:
+        print(f'Item: {item}')
+
+    # Concatenation
+    t2 = t * 3
+
+    for item in t2:
+        print(f'Item: {item}')
+
+    # Nested tuples
+    a = ((1, 2), (10, 20), (100, 200))
+    for l1 in a:
+        for l2 in l1:
+            print(f'Item 2: {l2}')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    # main()
+    items = (3, 88, 11, 22, 90)
+    lower, upper = minmax(items)
+    print(f'minimum {lower} and maximum {upper}')
+
+    # Test for membership: in, not in
+    if 3 in items:
+        print("I have a 3")
+    if 99 not in items:
+        print("I do not have 99")
